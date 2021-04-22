96f88d8983a3b8177555fcdce3af387f2dfc68f4
Vo-Ba-Nguyen <80742814+Vo-Ba-Nguyen@users.noreply.github.com>
2021-04-22 11:11:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js">
+</head>
+<body>
+    <div class="row py-5 px-4">
+        <div class="col-md-5 mx-auto">
+            <!-- Profile widget -->
+            <div class="bg-white shadow rounded overflow-hidden">
+                <div class="px-4 pt-0 pb-4 cover">
+                    <div class="media align-items-end profile-head">
+                     <a href="/Profile/image/70358564_706012969914300_3151589165775192064_n.jpg">  <div class="profile mr-3"><img src="/Profile/image/70358564_706012969914300_3151589165775192064_n.jpg" alt="..." width="130" class="rounded mb-2 img-thumbnail"></a> <a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit profile</a></div>
+                        <div class="media-body mb-5 text-white">
+                            <h4 class="mt-0 mb-0">Vo Ba Nguyen</h4>
+                            <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>New York</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-light p-4 d-flex justify-content-end text-center">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <h5 class="font-weight-bold mb-0 d-block">100</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
+                        </li>
+                        <li class="list-inline-item">
+                            <h5 class="font-weight-bold mb-0 d-block">500</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
+                        </li>
+                        <li class="list-inline-item">
+                            <h5 class="font-weight-bold mb-0 d-block">200</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
+                        </li>
+                    </ul>
+                </div>
+                <div class="px-4 py-3">
+                    <h5 class="mb-0">About</h5>
+                    <div class="p-4 rounded shadow-sm bg-light">
+                        <p class="font-italic mb-0">Web Developer</p>
+                        <p class="font-italic mb-0">Lives in DaNang City, VietNam</p>
+                        <p class="font-italic mb-0">Single</p>
+                    </div>
+                </div>
+                <div class="py-4 px-4">
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h5 class="mb-0">Recent photos</h5><a href="#" class="btn btn-link text-muted">Show all</a>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 mb-2 pr-lg-1"><a href="/Profile/image/TTP_0290.jpg"> <img src="/Profile/image/TTP_0290.jpg" alt="Ảnh" class="img-fluid rounded shadow-sm"></div></a>
+                        <div class="col-lg-6 mb-2 pl-lg-1"><a href="/Profile/image/69874613_668782013607249_2548269440349241344_n.jpg"><img src="/Profile/image/69874613_668782013607249_2548269440349241344_n.jpg " alt="Ảnh" class="img-fluid rounded shadow-sm"></div></a>
+                        <div class="col-lg-6 pr-lg-1 mb-2"><a href="/Profile/image/70358564_706012969914300_3151589165775192064_n.jpg"><img src="/Profile/image/70358564_706012969914300_3151589165775192064_n.jpg" alt="Ảnh" class="img-fluid rounded shadow-sm"></div></a>
+                        <div class="col-lg-6 pl-lg-1"><a href="/Profile/image/69062409_2170077003290985_1428346976931414016_n.jpg"><img src="/Profile/image/69062409_2170077003290985_1428346976931414016_n.jpg" alt="Ảnh" class="img-fluid rounded shadow-sm"></div></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <style>
+        .profile-head {
+    transform: translateY(5rem)
+}
+
+.cover {
+    background-image: url(/Profile/image/ảnh\ đám\ cưới.jpg);
+    background-size: cover;
+    background-repeat: no-repeat
+}
+
+body {
+    background: #654ea3;
+    background: linear-gradient(to right, #e96443, #904e95);
+    min-height: 100vh
+}
+h4{
+    color: black;
+}
+    </style>
+</body>
+</html>
